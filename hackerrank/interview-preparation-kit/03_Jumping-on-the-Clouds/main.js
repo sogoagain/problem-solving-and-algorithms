287d61cd1ab1a930df747f04f3e4c659ae5f9371
sogoagain <imyong0@gmail.com>
2020-09-01 22:11:50 +0900

    Implement - HackerRank Jumping on the Clouds

---
+++ hackerrank/interview-preparation-kit/03_Jumping-on-the-Clouds/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+    const clouds = [...c];
+    let position = 0;
+    let count = 0;
+
+    while (position < clouds.length - 1) {
+        const step = clouds[position + 2] === 1 ? 1 : 2;
+        position = position + step;
+        count++;
+    }
+
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
